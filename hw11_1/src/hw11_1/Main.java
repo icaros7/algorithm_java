1ccaf5c9116fb0a0631b63151d08244144616ace
iCAROS7 <hominlab@gmail.com>
2022-11-25 02:16:47 +0900

    A: Add hw11_1

---
+++ hw11_1/src/hw11_1/Main.java
@@ -0,0 +1,76 @@
+/*
+ * 이름: 이호민
+ * hw : 11_1
+ * 학번: 201735030
+ * 생성: 2022.11.25 01:15
+ */
+
+package hw11_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+
+public class Main {
+    private static class Meeting {
+        int _sTime, _eTime;     // 회의 시작 시간, 종료 시간
+
+        /**
+         * Meetings Class 생성자
+         * @param sTime 회의 시작 시간
+         * @param eTime 회의 종료 시간
+         */
+        public Meeting(int sTime, int eTime) {
+            this._sTime = sTime;
+            this._eTime = eTime;
+        }
+
+        @Override
+        public String toString() { return "(" + _sTime + ", " + _eTime + ")"; }
+    }
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());            // 전체 case 수 입력 받기
+        int before = 0;                                     // 마지막 _eTime 저장
+        Meeting[] meetings = new Meeting[n];                // 전체 meetings 리스트
+        ArrayList<Meeting> selMeetings = new ArrayList<>(); // 선택된 meetings 리스트
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            meetings[i] = (new Meeting(
+                    Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken())
+            ));
+        }
+
+        // 종료시각-시작시각 기준으로 오름차순 정렬
+        Arrays.sort(meetings, (o1, o2) -> {
+            if (o1._eTime == o2._eTime) { return (o1._sTime - o2._sTime); }
+            else { return (o1._eTime - o2._eTime); }
+        });
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                before = meetings[i]._eTime;
+                selMeetings.add(meetings[i]);
+            }
+            else {
+                if (before <= meetings[i]._sTime) {
+                    before = meetings[i]._eTime;
+                    selMeetings.add(meetings[i]);
+                }
+            }
+        }
+
+        sb.append(selMeetings.size()).append("\n");
+
+        System.out.print(sb);
+    }
+}
